ce5a377c8b54cd62b903f2d7cec50c43fb8ef1c4
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2022-10-01 18:06:31 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaCurso/AppGuiaCurso/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppGuiaCurso
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppGuiaCurso/AppGuiaCurso/Model/Componente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGuiaCurso.Model
+{
+    internal class Componente
+    {
+    }
+}
